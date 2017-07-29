f62c2aa4acd22fe011f6a4424420cc063c8e6343
Chester Ni <kaichesterni@gmail.com>
2017-07-29 21:54:52 +0000

    Added more test driver code and fixed capitalization

--- playlist.js
+++ playlist.js
@@ -18,22 +18,29 @@ class Playlist {
   }
 
   // add a new track to this playlist
-  addtrack(track) {
+  addTrack(track) {
     this.tracks.push(track);
   }
 
   // delete a track from this playlist
-  deletetrack(track) {
+  deleteTrack(track) {
     const id = this.tracks.indexOf(track);
     this.tracks.splice(id, 1);
   }
 
   // list all tracks in this playlist
-  listtracks() {
+  listTracks() {
     return this.tracks;
   }
 }
 
-// test driver
+// test driver - new playlist
 const testPlaylist = new Playlist("Test Playlist", "kaichesterni", "90s Hip Hop", ["California Love", "Nuthin' But A G Thang", "Gin & Juice", "It Was A Good Day"]);
 console.log(testPlaylist);
+
+// test driver - playlist functions
+console.log(testPlaylist.listTracks());
+testPlaylist.addTrack("Gangsta's Paradise");
+console.log(testPlaylist.listTracks());
+testPlaylist.deleteTrack("Gin & Juice");
+console.log(testPlaylist.listTracks());
